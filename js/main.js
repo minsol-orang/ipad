80a62fbc630db583dc20c6911779483089d32e03
minsol-orang <minsol030507@naver.com>
2026-07-28 01:01:50 +0900

    feat: 헤더 검색바 순차 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -31,10 +31,12 @@ function hideBasket() {
 
 // 검색
 const headerEl = document.querySelector('header')
+const headerMenuEls = [...headerEl.querySelectorAll('.ul.menu > li')]
 const searchWrapEl = headerEl.querySelector('.search-wrap')
 const searchStarterEl = headerEl.querySelector('.search-starter')
 const searchCloserEl = searchWrapEl.querySelector('.search-closer')
 const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 searchStarterEl.addEventListener('click', showSearch)
 searchCloserEl.addEventListener('click', hideSearching)
@@ -44,7 +46,22 @@ function showSearch() {
   headerEl.classList.add('searching')
   // documentElement = 해당 문서의 최상위 요소를 의미(<html></html>) 
   document.documentElement.classList.add('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    // 최대 .4초를 설정하기 위해서 .4를 곱해준 후 요소의 개수만큼을 나눠줌 (0~ 0.4)
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's' 
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's' 
+  })
 }
 function hideSearching() {
   headerEl.classList.remove('searching')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    // 최대 .4초를 설정하기 위해서 .4를 곱해준 후 요소의 개수만큼을 나눠줌 (0~ 0.4)
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's' 
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's' 
+  })
+  searchDelayEls.reverse()
 }
