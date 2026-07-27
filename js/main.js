1e09de50882ca0afbfc80c22d21e0f14a20fd11e
minsol-orang <minsol030507@naver.com>
2026-07-28 04:24:28 +0900

    feat: 서브 네비게이션 바 UI 및 호버 스타일 구현

--- js/main.js
+++ js/main.js
@@ -36,6 +36,7 @@ const searchWrapEl = headerEl.querySelector('.search-wrap')
 const searchStarterEl = headerEl.querySelector('.search-starter')
 const searchCloserEl = searchWrapEl.querySelector('.search-closer')
 const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchInputEl = searchWrapEl.querySelector('input')
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 searchStarterEl.addEventListener('click', showSearch)
@@ -52,7 +53,11 @@ function showSearch() {
   })
   searchDelayEls.forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / searchDelayEls.length + 's' 
-  })
+  }),
+  // .6초 뒤에 검색바가 완전히 나타나므로 focus를 .6초 뒤에 실행해주는 함수 setTimeout 사용
+  setTimeout(function() {
+    searchInputEl.focus()
+  }, 600)
 }
 function hideSearching() {
   headerEl.classList.remove('searching')
@@ -64,4 +69,6 @@ function hideSearching() {
     el.style.transitionDelay = index * .4 / searchDelayEls.length + 's' 
   })
   searchDelayEls.reverse()
+  // 검색바가 사라질 때 input 요소도 함께 초기화 시킴 
+  searchInputEl.value = ""
 }
